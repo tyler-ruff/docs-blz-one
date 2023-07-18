0ddf65602b7a0334c1d99e0bfe982a7e8ed8a95c
Tyler Ruff <hello@blazed.space>
2023-07-18 04:19:38 -0400

    w

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  outDir: '../public',
+  vite: {
+    
+  },
   title: "Blazed One Docs",
   description: "Blazed One Documentation",
   themeConfig: {
